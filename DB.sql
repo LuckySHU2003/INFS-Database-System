9f1f8c4da0093c5a97e05241da076da931c16a07
LuckySHU2003 <Luckyshu2003@gmail.com>
2024-09-10 15:17:21 +1000

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,4 +1,11 @@
-```sql
 *.sql linguist-language=PostgreSQL
 
 CREATE DATABASE customer_db
+WITH
+	OWNER = Lucky
+	LC_COLLATE = 'C'
+	LC_CTYPE = 'C'
+	TABLESPACE = pg_default
+	CONNECTION LIMIT = -1;
+
+\l customer_db
